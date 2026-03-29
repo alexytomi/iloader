e8d639cc48764dc548f36aedd5cd687c503bfee8
alexytomi <60690056+alexytomi@users.noreply.github.com>
2026-03-29 19:42:29 +0800

    Disable Tauri auto-update
    We can't have the user auto-updating

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -25,7 +25,6 @@ use tracing_subscriber::{Layer, Registry, fmt, layer::SubscriberExt, util::Subsc
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_process::init())
-        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_store::Builder::new().build())
